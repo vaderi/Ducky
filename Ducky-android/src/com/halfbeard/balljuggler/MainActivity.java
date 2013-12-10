b078559fffc89e2aa04db1a904ed24cbca0ddeb1
vaderi <vasily@middlebury.edu>
2013-12-09 19:16:39 -0500

    Renamed package

---
+++ Ducky-android/src/com/halfbeard/balljuggler/MainActivity.java
@@ -0,0 +1,21 @@
+package com.halfbeard.balljuggler;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.halfbeard.balljuggler.DuckyLab;
+
+public class MainActivity extends AndroidApplication
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = true;
+
+        initialize(new DuckyLab(), cfg);
+    }
+}
